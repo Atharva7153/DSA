ea146a518cf6201f392ac87e00ab5e8e5cc7f85f
Atharva7153 <atharvas7153@gmail.com>
2025-08-03 09:59:11 +0530

    Kadane's Algo

---
+++ arrays/Kadane'sAlgo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0, maxSum = INT_MIN;
+
+        for(int val : nums){
+
+            currSum += val;
+            maxSum = max(currSum, maxSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << "Maximum subarray sum: " << solution.maxSubArray(nums) << endl; // Output: 6
+    return 0;
+}
